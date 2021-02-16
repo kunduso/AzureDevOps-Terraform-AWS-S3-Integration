1567dc7aa1117f9f88e2311b7227da78ad2f452b
Sourav Kundu <skundu.dev@gmail.com>
2021-02-16 06:26:19 -0600

    removed reference to secret acess variables

--- variables.tf
+++ variables.tf
@@ -1,15 +1,15 @@
-  variable "region" {
+variable "region" {
     description = "The region where to provision resources"
     type = string
 }
-variable "access_key" {
-    description = "The access_key that belongs to the IAM user"
-    type = string
-}
-variable "secret_key" {
-    description = "The secret_key that belongs to the IAM user"
-    type = string
-}
+# variable "access_key" {
+#     description = "The access_key that belongs to the IAM user"
+#     type = string
+# }
+# variable "secret_key" {
+#     description = "The secret_key that belongs to the IAM user"
+#     type = string
+# }
 variable "bucket_name" {
     description = "The name of S3 bucket"
     default = "terraform-bucket"
